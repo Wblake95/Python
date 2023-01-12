12c2b611f1cb45a81b5cfcd3375cb9a60f09ef98
Wblake95 <wesleyblake95@gmail.com>
2023-01-12 13:23:39 -0800

    playing with the idea of creating a banking thing

--- banking.py
+++ banking.py
@@ -1,20 +1,34 @@
 #! /bin/env/ python
-def Balance():
-    nameUser = None
-    ageUser = None
-    passwordUser = None
-    Balance = 500
 
 def WhatToDo():
+    global Balance
     if check == 'c':
-        nameUser = input('What will be your username? ')
+        nameUser = input('What will be your username?: ')
         print(nameUser)
-        ageUser = input('What is your age? ')
-        passwordUser = input('Create a password ')
-    if check == 'd':
-        deposit = input('How much would you like to deposit? ')
-        deposit.int()
+        ageUser = input('What is your age?: ')
+        passwordUser = input('Create a password: ')
+        print(nameUser)
+        print(ageUser)
+        print(Balance)
+    elif check == 'd':
+        deposit = input('How much would you like to deposit?: ')
         Balance += int(deposit)
+        print(Balance)
+        return Balance
+    elif check == 'w':
+        withdraw = input('What would you like to withdraw?: ')
+        Balance -= int(withdraw)
+        print(Balance)
+        return Balance
+    elif check == 'h':
+        print(Balance)
 
-check = input('What would you like to do? ([C]reate account, [D]eposit money, [W]ithdraw money, or C[H]eck your balance)')
-check.casefold()
+
+while True:
+    Balance = 500
+    check = input('What would you like to do? ([C]reate account, [D]eposit money, [W]ithdraw money, or C[H]eck your balance): ')
+    check.casefold()
+    WhatToDo()
+    goAgain = input('y, n: ')
+    if goAgain == 'n':
+        break
