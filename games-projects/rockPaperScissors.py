acfb11800fe3a51c7e7240a4bb5c0311d6c40119
wblake95 <wesleyblake95@gmail.com>
2023-07-11 12:40:44 -0700

    playing with logic for rock paper scissors

--- games-projects/rockPaperScissors.py
+++ games-projects/rockPaperScissors.py
@@ -7,10 +7,12 @@ def gameCheck():
     global win,loss,tie,playerGuess,cpuChoice
     if playerGuess==gameList.index(cpuChoice):
         tie+=1
-    elif playerGuess<gameList.index(cpuChoice):
+    elif playerGuess==gameList.index['Rock'] and cpuChoice==gameList[0]:
         loss+=1
-    else:
+    elif playerGuess==gameList.index['Paper'] and cpuChoice==gameList[2]:
         win+=1
+    elif playerGuess>cpuChoice:
+        loss+=1
 # print(gameList.index('Paper'))
 # print(gameList[0])
 
