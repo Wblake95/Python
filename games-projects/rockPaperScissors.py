03a070606e0afc07dda0e44198d3eb1517963dfd
wblake95 <wesleyblake95@gmail.com>
2023-07-11 13:59:30 -0700

    Fixed logic for rockPaperScissors.py

--- games-projects/rockPaperScissors.py
+++ games-projects/rockPaperScissors.py
@@ -1,31 +1,31 @@
 #! /bin/env python
 
 import random
-gameList=['Paper','Scissors','Rock']
+gameList=['Rock','Paper','Scissors']
 win,loss,tie = 0,0,0
 def gameCheck():
-    global win,loss,tie,playerGuess,cpuChoice
-    if playerGuess==gameList.index(cpuChoice):
-        tie+=1
-    elif playerGuess==gameList.index['Rock'] and cpuChoice==gameList[0]:
-        loss+=1
-    elif playerGuess==gameList.index['Paper'] and cpuChoice==gameList[2]:
-        win+=1
-    elif playerGuess>cpuChoice:
-        loss+=1
-# print(gameList.index('Paper'))
-# print(gameList[0])
-
+        global win, loss, tie
+        if playerGuess=='Rock' and cpuChoice=='Scissors':
+            win+=1
+        elif playerGuess=='Scissors' and cpuChoice=='Paper':
+            win+=1
+        elif playerGuess=='Paper' and cpuChoice=='Rock':
+            win+=1
+        elif playerGuess==cpuChoice:
+            tie+=1
+        else:
+            loss+=1
 while True:
     print('Win:',win,'Loss:',loss,'Tie:',tie)
     cpuChoice=random.choice(gameList)
-    playerGuess=int(input('Paper[1], Scissors[2], Rock[3] '))
-    playerGuess-=1
+    playerGuess=gameList[int(input('Paper[1], Scissors[2], Rock[3] '))-1]
+    # playerGuess-=1
+    # playerGuess=gameList[playerGuess]
     gameCheck()
-    print('Player guess:',gameList[playerGuess],'\ncpu guess:', cpuChoice)
+    print('Player guess:',playerGuess,'\ncpu guess:', cpuChoice)
     if win>loss and win>=2:
         print('Great job, you won!')
         break
-    elif loss==2:
+    elif loss>win and loss>=2:
         print('You lost! Try again!')
         break
