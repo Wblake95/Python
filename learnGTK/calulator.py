616c422597c46e29451ce9cac94d11e5818387c7
wblake95 <wesleyblake95@gmail.com>
2023-08-06 11:56:03 -0700

    "Added the function calls for future button logic."

--- learnGTK/calulator.py
+++ learnGTK/calulator.py
@@ -6,19 +6,31 @@ from gi.repository import Gtk
 class Calculator(Gtk.Window):
     def __init__(self):
         super().__init__(title="Calculator")
-        # self.box = Gtk.Box(spacing=6)
-        # self.add(self.box)
+        self.set_border_width(10)
+
+        headerBar = Gtk.HeaderBar()
+        headerBar.set_show_close_button(True)
+        headerBar.props.title = "Calculator"
+        self.set_titlebar(headerBar)
 
         button1 = Gtk.Button(label="1")
         # self.button1.connect("clicked", self.on_button1_clicked)
         button2 = Gtk.Button(label="2")
+        # self.button2.connect("clicked", self.on_button2_clicked)
         button3 = Gtk.Button(label="3")
+        # self.button3.connect("clicked", self.on_button3_clicked)
         button4 = Gtk.Button(label="4")
+        # self.button4.connect("clicked", self.on_button4_clicked)
         button5 = Gtk.Button(label="5")
+        # self.button5.connect("clicked", self.on_button5_clicked)
         button6 = Gtk.Button(label="6")
+        # self.button6.connect("clicked", self.on_button6_clicked)
         button7 = Gtk.Button(label="7")
+        # self.button7.connect("clicked", self.on_button7_clicked)
         button8 = Gtk.Button(label="8")
+        # self.button8.connect("clicked", self.on_button8_clicked)
         button9 = Gtk.Button(label="9")
+        # self.button9.connect("clicked", self.on_button9_clicked)
 
         button0 = Gtk.Button(label="0")
         buttonSign = Gtk.Button(label="-/+")
@@ -28,7 +40,7 @@ class Calculator(Gtk.Window):
         buttonAdd = Gtk.Button(label="+")
         buttonSubtract = Gtk.Button(label="-")
         buttonEnter = Gtk.Button(label="=")
-        
+
         grid = Gtk.Grid()
         grid.add(button1)
         grid.attach(button2,1,0,1,1)
