e31fb9084b7d9279d026c67782d988140f55581a
wblake95 <wesleyblake95@gmail.com>
2023-08-20 11:36:43 -0700

    Playing with clipboard, have to usewayland, no python module I could find so far.

---
+++ Practice/clipboardRegex.py
@@ -0,0 +1,8 @@
+# I want to copy something from my clipboard and find emails;phone numbers from it.
+# 1. Get thetext off the clipboard
+#   - Find out what clipboard I have
+#   - How to access that clipboard
+# 2. Find all phone numbers and email addressses in the clipboard
+# 3. Paste them back into the clipboard
+
+# Pyperclip is not an option (wayland is not supported). Use Subprocess to run wl-copy/wl-paste
