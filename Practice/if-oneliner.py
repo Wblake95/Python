928e891c2717ebb4b2db6b4b8b016c3878b73515
wblake95 <wesleyblake95@gmail.com>
2023-08-23 22:09:14 -0700

    Moved items around to appropriate places

--- Practice/if-oneliner.py
+++
@@ -1,3 +0,0 @@
-x = "0"
-x = "1" if False else "2"
-print(x)
--- Practice/regexpress.py
+++
@@ -1,8 +0,0 @@
-import re
-
-batRegex = re.compile(r'Bat(wo)?man')
-mo1 = batRegex.search('The Adventures of Batman')
-mo1.group()
-
-mo2 = batRegex.search('The Adventures of Batwoman')
-mo2.group()
