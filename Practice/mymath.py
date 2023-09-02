3a514ebf853ab93cb95c5325fa6fafaaaa1ee664
wblake95 <wesleyblake95@gmail.com>
2023-09-02 10:13:47 -0700

    This is playing around with basic math logic

--- Practice/mymath.py
+++ Practice/mymath.py
@@ -1,19 +1,33 @@
 # the algorithm for sqaure root is a binary search
 def multi(num1,num2):
     sum = 0
+    if num2 > num1:
+        temp, num1 = num1, num2
+        num1 = temp
     for i in range(num2):
         sum = num1 + num1
         return sum
 
 def div(num1,num2):
     sum = 0
-    for i in range(num2):
-        sum = num1 - num1
-        return sum
+    count = 0
+    while True:
+        num1 = num1 - num2
+        count += 1
 
-def square(base):
-    # binary
-    for i in range(base):
-        num = base/2
-        if multi(num, num) == base:
-            return num
+def mysqrt(num1):
+    hi = 0
+    low = 0
+    temp = num1
+    while True:
+        if multi(temp,temp) > num1:
+            hi = temp
+            temp = temp//2
+        elif multi(temp,temp) < num1:
+            low = temp
+            temp = (hi+low)//2
+        elif multi(temp,temp) == num1:
+            return temp
+            break
+        else:
+            print("Something went wrong")
