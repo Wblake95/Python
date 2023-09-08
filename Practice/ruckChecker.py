fd3a144bbb4774390b1626e9fc9cee770aec1b1c
wblake95 <wesleyblake95@gmail.com>
2023-09-08 13:19:22 -0700

    more adventofcode

--- Practice/ruckChecker.py
+++ Practice/ruckChecker.py
@@ -1,10 +1,29 @@
 file = open("temp.txt")
 
-dict = {}
+tally = {}
 
-for i in range(97,97+26):
-    dict[chr(i)] = i-96
-for i in range(65,65+26):
-    dict[chr(i)] = i-38
+# lower case letters = score 1-26
+for i in range(97,(97+26)):
+    tally[chr(i)] = (i-96)
+# upper case letters = score 27-52
+for i in range(65,(65+26)):
+    tally[chr(i)] = (i-38)
 
+score = 0
 
+for i in file:
+    # grabs line with is a string
+    if not i:
+        break
+    for j in i:
+        # literally create temp
+        temp = j
+        count = 0
+        for h in i:
+            # compare temp to char in string,
+            # if count > 1, break
+            if temp == h:
+                count +=1
+            if count > 1:
+                score += tally[h]
+                break
