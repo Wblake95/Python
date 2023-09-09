50af822b0ab9cd3fba12a1f7eda7e9efe3f6ff12
wblake95 <wesleyblake95@gmail.com>
2023-09-08 19:36:11 -0700

    working on problem, my answer is to low.

--- Practice/4advent.py
+++ Practice/4advent.py
@@ -1,3 +1,4 @@
+from time import sleep
 file = open("temp.txt")
 
 spliter = []
@@ -16,12 +17,21 @@ del temp, spliter
 file.close()
 
 score = 0
+
 for i in list:
     if int(i[0][0]) >= int(i[1][0]):
         if int(i[0][1]) <= int(i[1][1]):
+            # print(i)
+            # print("left in right")
             score += 1
-    if int(i[1][0]) >= int(i[0][0]):
+            # print(score)
+            # sleep(1)
+    elif int(i[1][0]) >= int(i[0][0]):
         if int(i[1][1]) <= int(i[0][1]):
+            # print(i)
+            # print("right in left")
             score += 1
+            # print(score)
+            # sleep(1)
 
 print(score)
