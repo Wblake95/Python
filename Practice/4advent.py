890115bc61958f349d723512278677f72f569ef3
wblake95 <wesleyblake95@gmail.com>
2023-09-09 11:51:37 -0700

    Found an answer to my question from the reddit. I had no idea that people were so nice over there.

--- Practice/4advent.py
+++ Practice/4advent.py
@@ -1,37 +1,35 @@
-from time import sleep
 file = open("temp.txt")
 
 spliter = []
 temp = ""
 list = []
 
-for i in file:
-    if i == "\n":
+for i in file:# gets each line
+    if i == "\n":# skip last line in file
         break
-    spliter = i.split(",")
-    for i in range(len(spliter)):
+    spliter = i.split(",")# first split between elve pair
+    for i in range(len(spliter)):# I want each elf to be a list pair combo example: [[23,23],[23,34]]
         temp = spliter[i]
         spliter[i] = temp.split("-")
     list.append(spliter)
-del temp, spliter
-file.close()
 
-score = 0
+del temp, spliter# I don't need them anymore
+file.close()# don't need this anymore 
+score = 0# my scoring system
 
-for i in list:
+for i in list:# the actual work
     if int(i[0][0]) >= int(i[1][0]):
         if int(i[0][1]) <= int(i[1][1]):
-            # print(i)
-            # print("left in right")
             score += 1
-            # print(score)
-            # sleep(1)
-    elif int(i[1][0]) >= int(i[0][0]):
+    if int(i[1][0]) >= int(i[0][0]):
         if int(i[1][1]) <= int(i[0][1]):
-            # print(i)
-            # print("right in left")
             score += 1
-            # print(score)
-            # sleep(1)
+    if int(i[1][0]) == int(i[0][0]):
+        if int(i[1][1]) == int(i[0][1]):
+            score -= 1
+    # else:
+    #     if int(i[1][0]) >= int(i[0][0]):
+    #         if int(i[1][1]) <= int(i[0][1]):
+    #             score += 1
 
-print(score)
+print(score)# my answer is 516.
