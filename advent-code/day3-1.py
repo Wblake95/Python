4fb6d4c97fd79605dc41c04716883985845d2181
Wesley Blake <wesleyblake95@gmail.com>
2024-01-10 17:16:14 -0800

    working on day3 still

--- advent-code/day3-1.py
+++ advent-code/day3-1.py
@@ -1,43 +1,30 @@
 # my goal, locate the special characters and find the number around it.
 
-def myfunc(index, string):
-    global temp
-    other = ""
-    indexFound = False
-    target = string[index]
-    while True:
-        if string[index-1] in nums:
-            index -= 1
+def myFunc(index, string):
+    part = ""
+    if index > 0:
+        for x in string[index::-1]:
+            if x in listNums:
+                index -= 1
+            else:
+                index += 1
+                break
+    for x in range(index,len(string)):
+        if string[x] in listNums:
+            part = part + string[x]
         else:
             break
-    for x in range(index,len(string)):
-        if string[x] not in nums:
-            other = ""
-        if string[x] in nums:
-            other = other + string[x]
-        if string[x] == target: 
-            indexFound = True
-        if indexFound:
-            try:
-                if string[x+1] not in nums:
-                    temp.add(other)
-                    break
-                elif index == len(string)-1:
-                    temp.add(other)
-                    break
-            finally:
-                print("fail")
-                break
+    return part
 
-with open("data.txt", "r") as file:
+
+with open("temp.txt", "r") as file:
     score = 0
     table = {
             }
-    symbols = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+",",","<",">","/","?"]
-    nums = ["1","2","3","4","5","6","7","8","9","0"]
+    listSymbols = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+",",","<",">","/","?"]
+    listNums = ["1","2","3","4","5","6","7","8","9","0"]
     incr = 0
     temp = set()
-
     for i in file:
         table.update({incr:""})
         for j in i:
@@ -45,35 +32,48 @@ with open("data.txt", "r") as file:
                 break
             table[incr] = table[incr] + j
         incr += 1
-    for i in table:
-        for j in range(len(table[i])):
-            try:
+
+
+for i in range(len(table)):
+    for j in range(len(table[i])):
+        symbol = table[i][j]
+        strLen = len(table[i])-1
+        if symbol in listSymbols:
+            # up
+            if i > 0:
+                if table[i-1][j] in listNums:
+                    temp.add(myFunc(j,table[i-1]))
                 # left up
-                if table[i][j] in symbols and table[i-1][j-1] in nums:
-                    myfunc((j-1),table[i-1])
-                # up
-                if table[i][j] in symbols and table[i-1][j] in nums:
-                    myfunc(j,table[i-1])
+                if j > 0:
+                    if table[i-1][j-1] in listNums:
+                        temp.add(myFunc(j-1,table[i-1]))
                 # up right
-                if table[i][j] in symbols and table[i-1][j+1] in nums:
-                    myfunc((j+1),table[i-1])
-                # left
-                if table[i][j] in symbols and table[i][j-1] in nums:
-                    myfunc((j-1),table[i])
-                # right
-                if table[i][j] in symbols and table[i][j+1] in nums:
-                    myfunc((j+1),table[i])
+                if j < strLen:
+                    if table[i-1][j+1] in listNums:
+                        temp.add(myFunc(j+1,table[i-1]))
+            # left
+            if j > 0:
+                if table[i][j-1] in listNums:
+                    temp.add(myFunc(j-1,table[i]))
+            # right
+            if j < strLen:
+                if table[i][j+1] in listNums:
+                    temp.add(myFunc(j+1,table[i]))
+            # down
+            if i < strLen:
+                if table[i+1][j] in listNums:
+                    temp.add(myFunc(j,table[i+1]))
                 # left down
-                if table[i][j] in symbols and table[i+1][j-1] in nums:
-                    myfunc((j-1),table[i+1])
-                # down
-                if table[i][j] in symbols and table[i+1][j] in nums:
-                    myfunc(j,table[i+1])
+                if j > 0:
+                    if table[i+1][j-1] in listNums:
+                        temp.add(myFunc(j-1,table[i+1]))
                 # down right
-                if table[i][j] in symbols and table[i+1][j+1] in nums:
-                    myfunc((j+1),table[i+1])
-            finally:
-                None
-    for i in temp:
-        score += int(i)
-    print(score)
+                if j < strLen:
+                    if table[i+1][j+1] in listNums:
+                        temp.add(myFunc(j+1,table[i+1]))
+print(temp)
+for i in temp:
+    if i == '':
+        continue
+    score += int(i)
+print(score)
