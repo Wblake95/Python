b1f2c7be66deb232236b36da7df8036498b3003b
Wesley Blake <wesleyblake95@gmail.com>
2024-05-26 13:56:04 -0700

    Deleted old repository for datascience

--- Dive-Into-Data-Science/getData.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://bradfordtuckfield.com/hour.csv"
-
-response = requests.get(url)
-
-with open("myData.csv","wb") as file:
-    file.write(response.content)
-# This
--- README.md
+++
@@ -1,2 +0,0 @@
-# My python learning path.
-# Development goals
