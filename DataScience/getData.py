3ede48a7dc106a4122f37c5e696c45c6ad300ab8
Wesley Blake <wesleyblake95@gmail.com>
2024-05-27 12:30:37 -0700

    Added argument parser to getData.py, some logic to prevent overwriting (with flaw), moved main logic to function

--- DataScience/getData.py
+++ DataScience/getData.py
@@ -1,9 +1,41 @@
 #! /usr/bin/env python
 
-import requests
+import requests, argparse
+from os import listdir
 
-request = requests.get('https://bradfordtuckfield.com/hour.csv')
 
-with open('hours.csv','w') as file:
-    file.write(request.text)
+def logic():
+    # Fetching the data using the arguments
+    request = requests.get(args.url)
+    with open(args.filename,'w') as file:
+        file.write(request.text)
 
+
+# The argument parser starts
+parser = argparse.ArgumentParser()
+# creating arguments
+parser.add_argument('url',
+                    help="""URL to retrieve information.
+                    Example: https://yourURL.com/thing_you_want""")
+parser.add_argument('filename',
+                    help="Name of the file to save as.")
+# putting arguments in a variable
+args = parser.parse_args()
+# End arguments
+
+# Check to make sure file types are the same, default to url filetype
+if args.url[-4:] != args.filename[-4:]:
+    args.filename = args.filename.replace(args.filename[-4:], args.url[-4:])
+
+# If file already exists, ask for new name
+if args.filename in listdir():
+    overWrite = input("That file already exists, do you want to save over it? [y/n]: ").lower()
+    if overWrite == "y":
+        args.filename = args.filename.replace(args.filename[0:-4],input("new name: "))
+        logic()# I see the flaw here, you could save over another file
+    elif overWrite == "n":
+        print("Nothing happened, closing program.")
+    else:
+        print("Wrong entry, closing program.")
+else:
+    logic()
