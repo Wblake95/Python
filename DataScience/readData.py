79feb7abee1fe7856aa61376bcf2535fc729c55f
Wesley Blake <wesleyblake95@gmail.com>
2024-05-27 11:29:11 -0700

    Learning with readData.py

--- DataScience/readData.py
+++ DataScience/readData.py
@@ -4,11 +4,23 @@ dataFrame = pd.read_csv("hours.csv")
 
 #print(dataFrame.head())
 
-print("mean:\t","{:.2f}".format(dataFrame['count'].mean()))
-print("std:\t","{:.2f}".format(dataFrame['count'].std()))
-print("median:\t",dataFrame['count'].median())
-print("min:\t",dataFrame['count'].min())
-print("max:\t",dataFrame['count'].max())
+#print("mean:\t","{:.2f}".format(dataFrame['count'].mean()))
+#print("std:\t","{:.2f}".format(dataFrame['count'].std()))
+#print("median:\t",dataFrame['count'].median())
+#print("min:\t",dataFrame['count'].min())
+#print("max:\t",dataFrame['count'].max())
+#
+#print(dataFrame.describe())
+#
+#print(dataFrame.loc[2:4,"registered"])
 
 
-print(dataFrame.describe())
+#print(dataFrame.loc[dataFrame["hr"]<5,"registered"].mean())
+#print(dataFrame.loc[dataFrame["hr"]<5,"registered"].describe())
+
+#print("{:.2f}".format(dataFrame.loc[(dataFrame["hr"]<5) & (dataFrame["temp"]<0.50),"count"].mean()),"\n")
+#print(dataFrame.loc[(dataFrame["hr"]<5) & (dataFrame["temp"]<0.50),"count"].describe())
+
+#print(dataFrame.groupby(["season"])["count"].mean())
+
+print(dataFrame.groupby(["season","holiday"])["count"].mean())
